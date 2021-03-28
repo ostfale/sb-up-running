c8841396fda134f99b176d2c0a53bace867677b9
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-03-28 19:17:09 +0200

    Initial commit
    First commit for REST example project

---
+++ rest-demo/src/test/java/de/ostfale/sb/restdemo/RestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.sb.restdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

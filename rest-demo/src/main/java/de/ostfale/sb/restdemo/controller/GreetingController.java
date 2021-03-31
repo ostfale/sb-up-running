8b2bd8d5143c70c5c5a18200736fd689c0192e3e
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-03-31 10:31:01 +0200

    Properties using PropertyFile
    + added property file for type safety
    + fixed controller
    + enables annotation processing

---
+++ rest-demo/src/main/java/de/ostfale/sb/restdemo/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package de.ostfale.sb.restdemo.controller;
+
+import de.ostfale.sb.restdemo.config.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private final Greeting greeting;
+
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
+    @GetMapping
+    String getGreeting() {
+        return greeting.getName();
+    }
+
+    @GetMapping("/coffee")
+    String getNameAndCoffee() {
+        return greeting.getCoffee();
+    }
+}
